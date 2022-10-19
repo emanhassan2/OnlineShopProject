3913027a0691c1a6e7aec55c21719eaa572d8681
REDIET WOLDEMARIAM <redietwoldemariam@REDIETs-MacBook-Pro.local>
2022-10-19 07:57:27 -0500

    added spring project structure

---
+++ shoping spring project/src/main/java/edu/miu/cs425/onlineshoppingapp/OnlineShoppingappApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.onlineshoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShoppingappApplication.class, args);
+    }
+
+}
